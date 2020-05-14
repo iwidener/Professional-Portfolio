004db8192f6c1d38d99867a5379a951dec67b7e8
Inna Widener <iwide2019@gmail.com>
2020-05-14 15:57:20 -0500

    One card created. Images added.

---
+++ src/components/Card/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import img1 from "./img1.png";
+import "./style.css";
+
+function Card(props) {
+    return (
+
+        <div className="card">
+            <img src={img1} alt="Image 1" className="card-img-top" />
+
+            <div className="card-body text-dark">
+                <h6 className="title">Burger</h6>
+                <p className="card-text text-secondary">
+                    An app for the restaurants so that the users can easily input the names of burgers and devour them. </p>
+                <a href="https://github.com/iwidener/Burger" className="btn btn-success">GitHub Repo</a>
+                <a href="https://floating-eyrie-38357.herokuapp.com/" className="btn btn-success">Deployed Link</a>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,24 @@
-import React from"react";
+import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import Row from "../components/Row";
+import Column from "../components/Column";
+import Card from "../components/Card";
 
 function About() {
     return (
         <div>
             <Jumbotron>
-                
+                <Row>
+                    <div>
+                        <Column size="md-4">
+                            <Card />
+                        </Column>
+                    </div>
+                </Row>
             </Jumbotron>
-
         </div>
-    )
+
+    );
 }
 
 export default About;
