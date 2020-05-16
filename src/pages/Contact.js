e19c62f6f7a82c4c7f3ff849a8cc00d8d9b13f35
Inna Widener <iwide2019@gmail.com>
2020-05-15 22:33:49 -0500

    Navbar changed. Contact page created.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,22 @@
-import React from"react";
+import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import Row from "../components/Row";
+import Column from "../components/Column";
 
 function About() {
     return (
         <div>
             <Jumbotron>
-                
+                <Row>
+                    <Column size="md-12">
+                        <div className="text">
+                            <p>Please, contact me at iwide2019@gmail.com and I will reply you right away.
+                    Looking forward to hearing from you. Thank you for visiting my web page!</p>
+                        </div>
+
+                    </Column>
+                </Row>
+
             </Jumbotron>
 
         </div>
