4969a104efb3d98e513abe3c22142dc08e00028f
Inna Widener <iwide2019@gmail.com>
2020-05-16 11:55:10 -0500

    Updates

---
+++ precache-manifest.330ee611be31410839656d34bd7b768f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1599691b3ece8fd79e6faf153258e02b",
+    "url": "/Professional-Portfolio/index.html"
+  },
+  {
+    "revision": "88c053ec1c3b34370fac",
+    "url": "/Professional-Portfolio/static/css/main.9d8e39b4.chunk.css"
+  },
+  {
+    "revision": "43cf665d790b6fc73e0b",
+    "url": "/Professional-Portfolio/static/js/2.5223db0a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Professional-Portfolio/static/js/2.5223db0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c053ec1c3b34370fac",
+    "url": "/Professional-Portfolio/static/js/main.c5858da4.chunk.js"
+  },
+  {
+    "revision": "1895e7630238821d650a",
+    "url": "/Professional-Portfolio/static/js/runtime-main.7105e263.js"
+  },
+  {
+    "revision": "fcdce91b90a70c0dc6e9d78bee2f4b0d",
+    "url": "/Professional-Portfolio/static/media/bee.fcdce91b.jpg"
+  },
+  {
+    "revision": "0870008c0f84dfc012a3d7c059760e21",
+    "url": "/Professional-Portfolio/static/media/circuit.0870008c.png"
+  },
+  {
+    "revision": "d75e4907598777df5af50a99b422e71a",
+    "url": "/Professional-Portfolio/static/media/visual-presentation1.d75e4907.png"
+  },
+  {
+    "revision": "51af2315dc87fa3526f80d97b5fec786",
+    "url": "/Professional-Portfolio/static/media/visual-presentation2.51af2315.png"
+  },
+  {
+    "revision": "44c01d1012cf4fa8468d373c05490728",
+    "url": "/Professional-Portfolio/static/media/visual-presentation3.44c01d10.png"
+  },
+  {
+    "revision": "40a06cd91b00a2c987ac9ff1aa2569d1",
+    "url": "/Professional-Portfolio/static/media/visual-presentation4.40a06cd9.png"
+  },
+  {
+    "revision": "2acd607d19d155404c56fc4495466942",
+    "url": "/Professional-Portfolio/static/media/visual-presentation5.2acd607d.png"
+  }
+]);
